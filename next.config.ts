990aebca2c9b3ff4193def2e6235de6a5eb737b2
Miray Hirabayashi <miraykgungor@gmail.com>
2026-06-05 16:08:37 -0500

    chore(config): add new development origin to allowedDevOrigins in next configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  allowedDevOrigins: ['127.0.2.2'],
+  allowedDevOrigins: ['127.0.2.2', '192.168.0.103'],
 };
 
 export default nextConfig;
